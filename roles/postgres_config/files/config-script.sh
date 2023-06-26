6fcccdf34b1ff592ea60fbf542f1b127370b18fc
Yannick Evers <yannick-tim.evers@capgemini.com>
2023-06-26 11:21:39 +0200

    Reduce requests/limits, privileges configurable

--- roles/postgres_config/files/config-script.sh
+++ roles/postgres_config/files/config-script.sh
@@ -3,5 +3,5 @@ echo "Configuring PostgreSQL"
 echo "SELECT 'CREATE DATABASE $DB_NAME' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec" | psql -d postgres -w
 echo "SELECT 'CREATE USER $DB_USER' WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = '$DB_USER')\gexec" | psql -d postgres -w
 echo "ALTER USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS'" | psql -d postgres -w
-echo "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME to $DB_USER" | psql -d postgres -w
+echo "GRANT $DB_PRIVILEGES ON DATABASE $DB_NAME to $DB_USER" | psql -d postgres -w
 echo "$DB_CUSTOM_COMMAND" | psql -d postgres -w
